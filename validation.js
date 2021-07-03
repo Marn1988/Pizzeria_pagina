c3c726898b7a3ef759077e3c18034c0c04655419
Marn1988 <marn@LINDOS.localdomain>
2021-07-03 19:47:53 -0300

    se añade esxpresion regular para correo

--- validation.js
+++ validation.js
@@ -5,11 +5,29 @@ function validate(){
     calles= document.getElementById ("calles").value;
     numero_puerta= document.getElementById ("numero_puerta").value;
     ciudad= document.getElementById ("ciudad").value;
+    expresion= / \w+@\w\.+[a-z]/;
     if(nombre===""){
         alert("debe completar este campo")
         return false;
     }
-  
+    else if(telefono===""){
+        alert("debe completar este campo")
+        return false;
+    }
+    else if(calles===""){
+        alert("debe completar este campo")
+        return false;
 
+    }
+    else if(numero_puerta===""){
+        alert("debe completar este campo")
+        return false;
+    }
+    else if(calles===""){
+        alert("debe completar este campo")
+        return false;
+    }
+    /* agregar longitud maxima de los valores*/
+ /*if(nombre==="" || telefono==="" || calles==="" ||...) otra forma de reprecentar el codigo de arriba */
 
 }
