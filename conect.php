e8666a83eda63b2224092a1c26073652570a2ba8
Marn1988 <marn@LINDOS.localdomain>
2021-07-05 14:28:48 -0300

    envio de datos a base de datos (no funciona) error de al enviar datos

--- conect.php
+++ conect.php
@@ -1,8 +1,8 @@
 <?php
 $conexion = mysqli_connect("localhost","root","","bd_formulario");
-if(!$conexion){
-echo "Error al conectar a la base de datos";
-}
-else{
-    echo "conexion exitosa con base de datos";
-}
+//if(!$conexion){
+//echo "Error al conectar a la base de datos";
+//}
+//else{
+   // echo "conexion exitosa con base de datos";
+//}
--- datos.php
+++ datos.php
@@ -1,1 +1,24 @@
 <?php 
+// recibe datos de formulario
+include 'conect.php'; // incluye php de conexion a bd
+$nombre = $_POST["nombre"];
+$telefono = $_POST["telefono"];
+$calles = $_POST["calles"];
+$numero_puerta = $_POST["numero_puerta"];
+$ciudad = $_POST["ciudad"];
+// envia datos a la base de datos
+
+$insert= "INSERT INTO formulario ('nombre','telefono','calles,numero_puerta','ciudad') VALUES('$nombre','$telefono','$calles','$numero_puerta','$ciudad')";
+
+//ejecutar consulta
+
+$result= mysqli_query($conexion,$insert);
+
+if(!$result){
+    echo "error en el registro";
+}
+else{
+    echo "lísto tu registro esta completo";
+}
+//cerrar conexion
+mysqli_close($conexion);
